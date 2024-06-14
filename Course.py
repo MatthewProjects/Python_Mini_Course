1615f02be92a4e806787d611014033a7aaf27fbe
MatthewProjects <mvt0408@gmail.com>
2024-06-14 17:49:21 -0600

    Update Course.py

--- Course.py
+++ Course.py
@@ -1,17 +1,23 @@
-name = "John"
-food = "Pizza"
+name = 'John'
+food = 'Pizza'
 age = 30
 weight = 75.5
 is_student = True
+
+#Exercise - Creating Variables
 city = "Paris"
 year = 2020
 is_raining = True
 price = 19.99
+
+#Exercise - Creating Specific Variable Types:
 product_name = "Chocolate Chip Cookies"
 quantity_available = 24
 price_per_unit = 2.99
 is_in_stock = True
 
+
+#Exercise - Getting Data Types:
 print(type(name))
 print(type(food))
 print(type(weight))
@@ -25,14 +31,12 @@ print(type(quantity_available))
 print(type(price_per_unit))
 print(type(is_in_stock))
 
-
+#Exercise - Casting Variable Data Types:
 apples = 5 # Change to string
 print(type(apples))
 
 apples = str(apples)
 print(type(apples))
-
-
 
 bananas = '3' # Change to integer
 print(type(bananas))
@@ -40,15 +44,11 @@ print(type(bananas))
 bananas = int(bananas)
 print(type(bananas))
 
-
 price = 2.99 # Change to integer
 print(type(price))
 
 price = int(price)
 print(type(price))
-
-
-
 
 is_student = True # Change to string
 print(type(is_student))
@@ -56,13 +56,11 @@ print(type(is_student))
 is_student = str(is_student)
 print(type(is_student))
 
-
 pie = '3.14' # Change to float
 print(type(pie))
 
 pie = float(pie)
 print(type(pie))
-
 
 name = 'John'  # Initial non-numeric string assignment
 
@@ -72,30 +70,24 @@ if name.isdigit():
 else:
     print("The string cannot be converted to an integer.")  # Prints a message if the string cannot be converted
 
-
-
 age = 42 # Change to string
 print(type(age))
 age = str(age)
 print(type(age))
-
-
 
 off = 1 # Change to boolean
 print(type(off))
 off = (bool(off))
 print(type(off))
 
-
-
+#Exercise - Overwriting Variables:
 favorite_color = "blue"
 print(favorite_color)
 
 favorite_color = "purple"
 print(favorite_color)
 
-
-
+#Exercise - Modifying Strings:
 course_description = "The Python course is a comprehensive and hands-on learning experience designed to introduce students to the fundamentals of Python programming. Through a combination of interactive lectures, practical exercises, and real-world projects, participants will gain a solid understanding of Python syntax, data structures, control flow, functions, and object-oriented programming. They will also explore various libraries and modules for tasks such as data analysis, web scraping, and GUI development. By the end of the course, students will be equipped with the skills to develop their own Python applications and confidently pursue further programming endeavors"
 
 print(len(course_description))
