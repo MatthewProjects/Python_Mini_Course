3e222d5fb74221f04fe8607aa0f3afea321bc2f2
MatthewProjects <mvt0408@gmail.com>
2024-06-14 01:02:30 -0600

    Update Course.py

--- Course.py
+++ Course.py
@@ -111,18 +111,14 @@ num_sentences = len(sentences)
 print("Number of sentences:", num_sentences)
 
 
+new_course_description = "The Python course is a comprehensive and hands-on learning experience designed to introduce students to the fundamentals of Python programming. Through a combination of interactive lectures, practical exercises, and real-world projects, participants will gain a solid understanding of Python syntax, data structures, control flow, functions, and object-oriented programming. They will also explore various libraries and modules for tasks such as data analysis, web scraping, and GUI development. By the end of the course, students will be equipped with the skills to develop their own Python applications and confidently pursue further programming endeavors"
 
+# Replace "Python" with "JavaScript" and store the result
+javascript_description = new_course_description.replace("Python", "JavaScript")
 
-
-modified_description = course_description.replace("Python", "Javascript")
-
-modified_description = course_description.replace("programming", "coding")
+# Replace "programming" with "coding" in the modified string
+modified_description = javascript_description.replace("programming", "coding")
 
 print(modified_description)
-
-print(course_description.upper())
-
-print(course_description.lower())
-
 
 
