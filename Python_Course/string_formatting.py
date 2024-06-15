dfd9d0362cee0152622f44ed941fe24a26227aae
MatthewProjects <mvt0408@gmail.com>
2024-06-15 01:03:10 -0600

    Update string_formatting.py

--- Python_Course/string_formatting.py
+++ Python_Course/string_formatting.py
@@ -6,6 +6,7 @@ def main():
   language1 = "English"
   language2 = "Portuguese"
   
+  
   print(f"My name is {name}, I'm {age} years old, and I live in {city}, {country}. I speak {language1} & {language2}.")
 
 
