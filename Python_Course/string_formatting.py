65648a5a324806bec64f830a67f12b014a7d4ed3
MatthewProjects <mvt0408@gmail.com>
2024-06-15 19:19:43 -0600

    Update string_formatting.py

--- Python_Course/string_formatting.py
+++ Python_Course/string_formatting.py
@@ -5,13 +5,15 @@ def main():
   city = "Boulder"
   language1 = "English"
   language2 = "Portuguese"
-  
-  
+
+  # Define the quote variable
+  quote = "The only limit to our realization of tomorrow is our doubts of today."
+
+  # Print the initial information
   print(f"My name is {name}, I'm {age} years old, and I live in {city}, {country}. I speak {language1} & {language2}.")
 
-
-
-
+  # Print the quote with escape characters using f-string
+  print(f'In {country}, people say:\n\t"{quote}"')
 
 if __name__ == "__main__":
-    main()
+  main()
