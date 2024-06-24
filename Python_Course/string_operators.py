9ac5fca6de6fd648b11572d55347b680ed2c27e2
Matthew Troughton <mvt0408@gmail.com>
2024-06-23 19:01:37 -0600

    Update string_operators.py

---
+++ Python_Course/string_operators.py
@@ -0,0 +1,23 @@
+# Create two variables, str1 and str2
+# Define str1
+str1 = "Hello, "
+# Define str2
+str2 = "World"
+
+# Concatenate str1 and str2
+concat_result = str1 + str2
+
+# Repeat str1 3 times
+repetition_result = str1 * 3
+
+# Print the values of all the variables
+print("str1: ", str1)
+print("str2: ", str2)
+print("concat_result: ", concat_result)
+print("repetition_result: ", repetition_result)
+# Print the first character of str1
+print("The first character of str1 is: ", str1[0])
+# Printing our the last character of str2
+print("The last character of str2 is: ", str2[4])
+# Printing out the substring from 2nd to 5th character to console
+print()
