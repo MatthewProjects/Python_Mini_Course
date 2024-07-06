98c9c17e45e1a181dd9c29fc2e959cd171d995f2
Matthew Troughton <mvt0408@gmail.com>
2024-07-06 14:29:33 -0600

    Update booleans.py

--- Python_Course/booleans.py
+++ Python_Course/booleans.py
@@ -12,3 +12,14 @@ print(is_a_in_word)
 print(is_o_in_word)
 print(is_u_in_word)
 print(is_num1_not_equal_to_num2)
+
+# Comparisons
+print(5 == 5.0)       # True: Integer 5 is equal to float 5.0
+print(5 != 5.0)       # False: Integer 5 is equal to float 5.0
+print(5 == '5')       # False: Integer 5 is not equal to string '5'
+print([] != ())       # True: Empty list is not equal to empty tuple
+print([] == "")       # False: Empty list is not equal to empty string
+print([] == [])       # True: Empty list is equal to empty list
+print({} == {})       # True: Empty dictionary is equal to empty dictionary
+print({} == ())       # False: Empty dictionary is not equal to empty tuple
+print({} == 0)        # False: Empty dictionary is not equal to integer 0
