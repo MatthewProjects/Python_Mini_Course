fc0e6168877f86794ff0276e6bbbaf2f9422a569
Matthew Troughton <mvt0408@gmail.com>
2024-07-06 14:19:11 -0600

    Create booleans.py

---
+++ Python_Course/booleans.py
@@ -0,0 +1,14 @@
+num1 = 5
+num2 = -12.3
+is_num1_larger = num1 > num2
+word = "pizza"
+is_a_in_word = "a" in word
+is_o_in_word = "o" in word
+is_u_in_word = "u" in word
+is_num1_not_equal_to_num2 = num1 != num2
+
+print(is_num1_larger)
+print(is_a_in_word)
+print(is_o_in_word)
+print(is_u_in_word)
+print(is_num1_not_equal_to_num2)
